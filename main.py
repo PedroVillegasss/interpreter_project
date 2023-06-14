eeda1073bf29398ce7a8e9ebc0a8f8ce071180d0
PedroVillegasss <ppvillegas@miuandes.cl>
2023-06-14 11:39:06 -0400

    Revisarlo

--- main.py
+++ main.py
@@ -2,43 +2,44 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 reserved = {
-   'mas' : 'SU',
-   'menos' : 'RE',
-   'es' : 'ASIGN'
+    'add_transaction': 'ADD',
+    'set_budget': 'SET',
+    'savings_goal': 'GOAL',
+    'spending_report': 'REPORT',
 }
 
 tokens = [
     'N',
-    'ID'
-    ]+list(reserved.values())
-    
-t_SU = r'mas'
-t_RE = r'menos'
-t_ASIGN = r'es'
+    'ID',
+    'ASIGN'
+] + list(reserved.values())
+
+t_ASIGN = r'^es$'
 
 def t_N(t):
     r'[0-9]+'
     t.value = int(t.value)
     return t
-    
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')
     return t
-    
+
 t_ignore  = ' \t'
 
 def t_error(t):
     print(":(")
     t.lexer.skip(1)
 
-precedence = (('left', 'SU', 'RE'),)
-
 variables = {}
+transacciones = []
+presupuestos = {}
+ahorros = 0
 
 def p_resultado(t):
     'resultado : s'
-    print(t[1])
+    t[0] = t[1]
 
 def p_asignacion(t):
     'resultado : ID ASIGN s'
@@ -47,32 +48,59 @@ def p_asignacion(t):
 def p_expr_num(t):
     's : N'
     t[0] = t[1]
-    
+
 def p_expr_id(t):
     's : ID'
     try:
         t[0] = variables[t[1]]
     except LookupError:
-        print("Variable indefinida '%s'" % t[1])
+        print("Error, por favor revise la sintaxis del comando ingresado")
         t[0] = 0
-    
-def p_expr_op(t):
-    '''s : s SU s
-        | s RE s'''
-    if t[2] == 'mas':
-        t[0] = t[1]+t[3]
-    elif t[2] == 'menos':
-        t[0] = t[1]-t[3]
+
+def p_add_transaction(t):
+    's : ADD ID N'
+    transacciones.append((t[2], t[3]))
+    print("Se ha agregado una nueva transaccion")
+
+def p_set_budget(t):
+    's : SET ID N'
+    presupuestos[t[2]] = t[3]
+    print("Se ha establecido un presupuesto nuevo")
+
+def p_savings_goal(t):
+    's : GOAL N'
+    global ahorros
+    ahorros = t[2]
+    print("Se ha establecido una nueva meta de ahorro")
+
+def p_spending_report(t):
+    's : REPORT'
+    reporte = "Transaccion\tMonto\n--------------------\n"
+    for transaccion, monto in transacciones:
+        reporte += f"- {transaccion}\t\t{monto}\n"
+    reporte += "--------------------\n"
+    gastos_totales = sum([monto for _, monto in transacciones])
+    reporte += f"Gastos totales en transacciones: {gastos_totales}\n"
+    reporte += "Presupuestos actualizados despues de las transacciones > \n"
+    for categoria, presupuesto in presupuestos.items():
+        gastado = sum([monto for transaccion, monto in transacciones if transaccion == categoria])
+        diferencia = presupuesto - gastado
+        reporte += f"- {categoria}: {diferencia}\n"
+    reporte += "--------------------\n"
+    reporte += f"Meta de ahorro establecida en {ahorros}"
+    t[0] = reporte
 
 def p_error(t):
-    print(":'(")
+    print("Error, por favor revise la sintaxis del comando ingresado")
 
+lexer = lex.lex()
+parser = yacc.yacc()
 
-lexer=lex.lex()
-parser=yacc.yacc()
 while True:
     try:
         data = input()
     except EOFError:
         break
-    parser.parse(data)
+    result = parser.parse(data)
+    if result is not None:
+        print(result)
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftSUREASIGN ID N RE SUresultado : sresultado : ID ASIGN ss : Ns : IDs : s SU s\n        | s RE s'
+_lr_signature = 'ADD ASIGN GOAL ID N REPORT SETresultado : sresultado : ID ASIGN ss : Ns : IDs : ADD ID Ns : SET ID Ns : GOAL Ns : REPORT'
     
-_lr_action_items = {'ID':([0,5,6,7,],[3,9,9,9,]),'N':([0,5,6,7,],[4,4,4,4,]),'$end':([1,2,3,4,8,9,10,11,],[0,-1,-4,-3,-5,-4,-6,-2,]),'SU':([2,3,4,8,9,10,11,],[5,-4,-3,-5,-4,-6,5,]),'RE':([2,3,4,8,9,10,11,],[6,-4,-3,-5,-4,-6,6,]),'ASIGN':([3,],[7,]),}
+_lr_action_items = {'ID':([0,5,6,9,],[3,10,11,13,]),'N':([0,7,9,10,11,],[4,12,4,15,16,]),'ADD':([0,9,],[5,5,]),'SET':([0,9,],[6,6,]),'GOAL':([0,9,],[7,7,]),'REPORT':([0,9,],[8,8,]),'$end':([1,2,3,4,8,12,13,14,15,16,],[0,-1,-4,-3,-8,-7,-4,-2,-5,-6,]),'ASIGN':([3,],[9,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'resultado':([0,],[1,]),'s':([0,5,6,7,],[2,8,10,11,]),}
+_lr_goto_items = {'resultado':([0,],[1,]),'s':([0,9,],[2,14,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,10 +27,12 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> resultado","S'",1,None,None,None),
-  ('resultado -> s','resultado',1,'p_resultado','main.py',40),
-  ('resultado -> ID ASIGN s','resultado',3,'p_asignacion','main.py',44),
-  ('s -> N','s',1,'p_expr_num','main.py',48),
-  ('s -> ID','s',1,'p_expr_id','main.py',52),
-  ('s -> s SU s','s',3,'p_expr_op','main.py',60),
-  ('s -> s RE s','s',3,'p_expr_op','main.py',61),
+  ('resultado -> s','resultado',1,'p_resultado','main.py',41),
+  ('resultado -> ID ASIGN s','resultado',3,'p_asignacion','main.py',45),
+  ('s -> N','s',1,'p_expr_num','main.py',49),
+  ('s -> ID','s',1,'p_expr_id','main.py',53),
+  ('s -> ADD ID N','s',3,'p_add_transaction','main.py',61),
+  ('s -> SET ID N','s',3,'p_set_budget','main.py',66),
+  ('s -> GOAL N','s',2,'p_savings_goal','main.py',71),
+  ('s -> REPORT','s',1,'p_spending_report','main.py',76),
 ]
